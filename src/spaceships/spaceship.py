b1cfa5b6479086bbebbcc6da7344f57deee7f630
NamDo8467 <huynhnamdo@gmail.com>
2024-03-10 00:22:07 -0500

    adjust the parent class

--- src/spaceships/spaceship.py
+++ src/spaceships/spaceship.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame import Surface
-from src.Bullet import Bullet
-from main import SCREEN_WIDTH
+from src.bullet import Bullet
 
 """Abstract class spaceships (including enemy, boss and hero)"""
 class Spaceship:
@@ -9,28 +8,14 @@ class Spaceship:
         self.X:int = x
         self.Y:int = y
         self.name:str = name
-        # self.image:Surface = pygame.image.load("images/spaceship_red.png").convert_alpha()
-        if self.name == "hero":
-            print("I run")
-            self.spaceShipSurface: Surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_yellow.png").convert_alpha(), (50, 50)), 180)
+        # if self.name == "hero":
+        #     self.spaceShipSurface: Surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_yellow.png").convert_alpha(), (50, 50)), 180)
         
-    def draw(self, screen:Surface) -> None:
-        screen.blit(self.spaceShipSurface, (self.X, self.Y))
+    def draw(self, screen:Surface, spaceship_surface) -> None:
+        screen.blit(spaceship_surface, (self.X, self.Y))
         
-    def move(self,keys:list) -> None:
-        if keys[pygame.K_RIGHT] and self.X < SCREEN_WIDTH - 50:
+    def move(self,keys) -> None:
+        if keys[pygame.K_RIGHT] and self.X < 400 - 50: # 400 is the SCREEN_WIDTH and 50 is the width of the spaceship
            self.X += 10
         if keys[pygame.K_LEFT] and self.X > 0:
             self.X -= 10
-            
-
-    # def fire(self) -> Bullet:
-    #     # color_code = self.__color_list[randrange(0, len(self.__color_list))]
-    #     bullet = Bullet(self.X, self.Y)
-    #     # bullet = {"color": color_code, "shape": pygame.Rect(self.X + 20, self.Y - 25, 10, 10)}
-    #     return bullet
-    # def detect_collision(self, bullet:Bullet, wall):
-    #     for brick in wall.queue:
-    #         if bullet["shape"].colliderect(pygame.Rect(brick.x, brick.y, brick.width, brick.height)) \
-    #                 and bullet["color"] == brick.color:
-    #             return brick
