bbf584ba8a8e3e805a54199079bcfaa4c8729951
NamDo8467 <huynhnamdo@gmail.com>
2024-03-10 20:47:53 -0400

    modified the methods to make it own style

--- src/spaceships/enemy_spaceship.py
+++ src/spaceships/enemy_spaceship.py
@@ -2,9 +2,13 @@ from .spaceship import Spaceship
 import pygame
 from pygame import Surface
 class EnemySpaceship(Spaceship):
-	def __init__(self, name = "enemy", x = 200, y = 40) -> None:
+	def __init__(self, name = "enemy", x = 0, y = 0) -> None:
 		super().__init__(name, x, y)
-		self.spaceship_surface: Surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_red.png").convert_alpha(), (20, 20)), 0)
+		
 		
 	def draw(self, screen)->None:
-		super().draw(screen, self.spaceship_surface)
+		super().draw(screen)
+
+	def move(self, key = None)->None:
+		self.x += 2
+		self.y += 2
--- src/spaceships/hero_spaceship.py
+++ src/spaceships/hero_spaceship.py
@@ -4,11 +4,11 @@ import pygame
 class HeroSpaceship(Spaceship):
 	def __init__(self, name="hero", x = 0, y = 420)->None:
 		super().__init__(name, x, y)
-		self.spaceship_surface: Surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_yellow.png").convert_alpha(), (50, 50)), 180)
+		# self.spaceship_surface: Surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_yellow.png").convert_alpha(), (50, 50)), 180)
 
 
 	def draw(self, screen:Surface) -> None:
-		super().draw(screen, self.spaceship_surface)
+		super().draw(screen)
 
 	def move(self, keys) -> None:
 		if keys[pygame.K_RIGHT] and self.x < 400 - 50: # 400 is the SCREEN_WIDTH and 50 is the width of the spaceship
