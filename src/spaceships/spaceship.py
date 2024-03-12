75719bf2af99e74cf73835a0825b833684f8fe54
NamDo8467 <huynhnamdo@gmail.com>
2024-03-12 00:32:22 -0400

    refactored constructor's parameters

--- src/spaceships/spaceship.py
+++ src/spaceships/spaceship.py
@@ -4,7 +4,7 @@ from src.bullet import Bullet
 
 """Abstract class spaceships (including enemy, boss and hero)"""
 class Spaceship:
-    def __init__(self, name:str, x:int, y:int) -> None:
+    def __init__(self, x:int, y:int, name:str) -> None:
         self.x:int = x
         self.y:int = y
         self.name:str = name
@@ -25,8 +25,4 @@ class Spaceship:
         screen.blit(self.spaceship_surface, (self.x, self.y))
         
     def move(self, key) -> None:
-        # if keys[pygame.K_RIGHT] and self.x < 400 - 50: # 400 is the SCREEN_WIDTH and 50 is the width of the spaceship
-        #    self.x += 10
-        # if keys[pygame.K_LEFT] and self.x > 0:
-        #     self.x -= 10
         pass
