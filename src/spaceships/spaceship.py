f14d6d44394ec4da079e420db6a5a7439a627ca1
NamDo8467 <huynhnamdo@gmail.com>
2024-03-15 00:45:04 -0400

    created 2 rows of enemy spaceships but needs to figure out how to animate them

--- src/spaceships/spaceship.py
+++ src/spaceships/spaceship.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import Surface
 from src.bullet import Bullet
+from .spaceship_dimension import dimension
 
 """Abstract class spaceships (including enemy, boss and hero)"""
 class Spaceship:
@@ -8,11 +9,7 @@ class Spaceship:
         self.x:float = x
         self.y:float = y
         self.name:str = name
-        self._dimension: dict = {
-            "hero":(50,50),
-            "enemy":(30,30),
-            "boss": (80,80)
-        }
+        self._dimension: dict = dimension
         self.spaceship_surface:Surface
         if self.name == "hero":
             self.spaceship_surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_yellow.png").convert_alpha(), self._dimension["hero"]), 180)
