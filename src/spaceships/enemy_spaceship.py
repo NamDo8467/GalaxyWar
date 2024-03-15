ccf371ec3d749332adb072e246bd8461ab43e3a3
NamDo8467 <huynhnamdo@gmail.com>
2024-03-15 00:44:37 -0400

    modified move_x and move_y methods

--- src/spaceships/enemy_spaceship.py
+++ src/spaceships/enemy_spaceship.py
@@ -2,20 +2,29 @@ from .spaceship import Spaceship
 import pygame
 from pygame import Surface
 class EnemySpaceship(Spaceship):
-	def __init__(self, x:float = 0, y:float = 0, name:str = "enemy") -> None:
+	def __init__(self, x:float = 0, y:float = 0, name:str = "enemy", row:int = 1) -> None:
 		super().__init__(x,y,name)
+		self.row = row
 		
 		
 	def draw(self, screen)->None:
 		super().draw(screen)
 
-	def move_x_and_y(self, key = None)->None:
-		if self.x > 170: #170 is the boundary. If the enemy spaceship x-coordinate is bigger then the x-coordinate will be decreasing
-			self.x -= 2.5
+	def move_x_and_y(self, distance:float = 2.2)->None:
+		if self.x > 225: #225 is the boundary (middle point of the screen width). If the enemy spaceship x-coordinate is bigger then the x-coordinate will be decreasing
+			self.x -= distance
 		else:
-			self.x += 2.5
-		self.y += 1.5
+			self.x += distance
+		self.y += 2.2
 	def move_x(self) -> None:
 		self.x += 2.5
 	def move_y(self) -> None:
-		self.y += 1.5
+		self.y += 2.2
+
+	def move_x_by(self, distance:float) -> None:
+		if self.x > 225: #225 is the boundary (middle point of the screen width). If the enemy spaceship x-coordinate is bigger then the x-coordinate will be decreasing
+			self.x -= distance
+		else:
+			self.x += distance
+	def move_y_by(self, distance:float) -> None:
+		self.y += distance
