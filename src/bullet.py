b295861bad99af20c8ac004166ed7567961fc553
NamDo8467 <huynhnamdo@gmail.com>
2024-03-17 09:59:49 -0400

    created bullet class

--- src/bullet.py
+++ src/bullet.py
@@ -1,14 +1,14 @@
 import pygame
 from src.colors import colors
 from pygame import Surface
+from .spaceships.spaceship_dimension import dimension
 class Bullet:
-	def __init__(self, spaceShipX:int, spaceShipY:int):
-		self.x = 0
-		self.y = 0
+	def __init__(self, spaceShipX:float, spaceShipY:float):
+		self.x:float = spaceShipX + (dimension["hero"][0]/2-5)
+		self.y:float = spaceShipY - (dimension["hero"][0]/2-10)
 		self.color = list(colors.values())[0]# red
-		self.shape = pygame.Rect(spaceShipX + 20, spaceShipY - 23, 10, 10)
-
-	def draw(self, screen: Surface):
+		self.shape = pygame.Rect(self.x, self.y, 5, 15)
+	def draw(self, screen: Surface)->None:
 		# print("Yes")
 		# self.shape = pygame.Rect(spaceShipX + 20, spaceShipY - 23, 10, 10)
 		pygame.draw.rect(screen, self.color, self.shape)
