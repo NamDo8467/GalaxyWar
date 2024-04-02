15c8dc6ef6691917a9c02afb0074c4943f18fd78
NamDo8467 <huynhnamdo@gmail.com>
2024-04-01 23:37:19 -0400

    added return type to detect_collision method

--- src/spaceships/hero_spaceship.py
+++ src/spaceships/hero_spaceship.py
@@ -2,12 +2,13 @@ from .spaceship import Spaceship
 from pygame import Surface
 from src.bullet import Bullet
 from .enemy_spaceship import EnemySpaceship
+from .boss_spaceship import BossSpaceship
 from .spaceship_dimension import dimension
+from typing import Union
 import pygame
 class HeroSpaceship(Spaceship):
 	def __init__(self, x:float = 0, y:float = 420, name:str ="hero")->None:
 		super().__init__(x,y,name)
-		# self.spaceship_surface: Surface = pygame.transform.rotate(pygame.transform.scale(pygame.image.load("images/spaceship_yellow.png").convert_alpha(), (50, 50)), 180)
 		self.width:float = dimension["hero"][0]
 		self.height:float = dimension["hero"][1]
 
@@ -19,14 +20,12 @@ class HeroSpaceship(Spaceship):
 			self.x += 10
 		if keys[pygame.K_LEFT] and self.x > 0:
 			self.x -= 10
-		# if keys[pygame.K_UP] and self.y > 0:
-		# 	self.y -= 10
-		# if keys[pygame.K_DOWN] and self.y < 420:
-		# 	self.y += 10
+
 	def fire(self) -> Bullet:
 		return super().fire()
-	def detect_collision(self, bullet:Bullet, enemy_fleet: list) -> EnemySpaceship: # or return a BossSpaceship
+	
+	def detect_collision(self, bullet:Bullet, enemy_fleet: list) -> Union[EnemySpaceship, BossSpaceship, None]: # or return a BossSpaceship
 		for enemy in enemy_fleet:
 			if bullet.shape.colliderect(pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)):
 				return enemy
-            
+		return None
