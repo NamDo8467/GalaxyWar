e9df63e8e9427ca41748b29ce80ce26d8d4f25e7
NamDo8467 <huynhnamdo@gmail.com>
2024-04-01 23:36:41 -0400

    removed comments

--- src/spaceships/boss_spaceship.py
+++ src/spaceships/boss_spaceship.py
@@ -15,11 +15,9 @@ class BossSpaceship(Spaceship):
 		
 	def draw(self, screen:Surface)->None:
 		super().draw(screen)
-		# pygame.draw.rect(screen, (255,0,0), pygame.Rect(self.x, self.y, self.width, self.height),2)
 
 	def fire(self) -> Bullet:
 		x = randint(self.x, self.x + self.width)
-		# y = randint(self.y, self.height)
 		y = self.y + self.height
 		bullet = Bullet(x, y, "boss")
 		return bullet
@@ -27,7 +25,6 @@ class BossSpaceship(Spaceship):
 	def draw_health_bar(self, screen:Surface) -> None:
 		pygame.draw.rect(screen, (255,0,0), pygame.Rect(125, 45, 200, 20), 2) # no color-filled inside
 		pygame.draw.rect(screen, (255,0,0), pygame.Rect(125, 45, self.health_point, 20)) # color-filled inside
-		# pass
 	
 	def detect_collision(self, bullet:Bullet, hero_spaceship) -> bool:
 		if bullet.shape.colliderect(pygame.Rect(hero_spaceship.x, hero_spaceship.y, hero_spaceship.width, hero_spaceship.height)):
